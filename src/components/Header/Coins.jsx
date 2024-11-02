b9c12886363f8f47459cc3e6b40958bbe0bc946f
Vitor Hugo <vv001226@gmail.com>
2024-11-02 18:16:34 -0300

    Adicionando botoes e carrinho de compras no header

--- src/components/Header/Coins.jsx
+++ src/components/Header/Coins.jsx
@@ -1,12 +1,18 @@
+import { ShoppingCart } from 'lucide-react';
 import { Coins as CoinsIcon } from 'lucide-react';
 
 export function Coins() {
   return (
-    <div className="flex bg-zinc-100 px-4 py-2 border border-zinc-200 dark:border-zinc-700 dark:bg-zinc-800 rounded-md items-center space-x-2">
-      <CoinsIcon className="h-6 w-6 text-amber-600 dark:text-amber-400" />
-      <span className="text-lg text-amber-600 dark:text-amber-300">500</span>
+    <div className='flex gap-4'>
+      <div className="flex bg-zinc-100 px-4 py-2 border border-zinc-200 dark:border-zinc-700 dark:bg-zinc-800 rounded-md items-center space-x-2">
 
+        <CoinsIcon className="h-6 w-6 text-amber-600 dark:text-amber-400" />
+        <span className="text-lg text-amber-600 dark:text-amber-300">500</span>
+      </div>
+
+      <div className="flex bg-zinc-100 px-4 py-2 border border-zinc-200 dark:border-zinc-700 dark:bg-zinc-800 rounded-md items-center space-x-2">
+        <ShoppingCart className="h-6 w-6 text-amber-600 dark:text-amber-400" />
+      </div>
     </div>
-
   );
 }
--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,21 +1,41 @@
+import { Star } from 'lucide-react';
 import PropTypes from 'prop-types';
 
 Product.propTypes = {
   image: PropTypes.string.isRequired,
   product: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
-}
-export function Product({ image, product, price }) {
+  onBuy: PropTypes.func.isRequired,
+  onAddToCart: PropTypes.func.isRequired,
+  buyText: PropTypes.string,
+  addToCartText: PropTypes.string,
+};
+
+export function Product({
+  image,
+  product,
+  price,
+  onBuy,
+  onAddToCart,
+  buyText = "Comprar",
+  addToCartText = "Adicionar ao carrinho"
+}) {
   return (
-    <div className="flex justify-between flex-col gap-2 p-4 rounded-lg bg-zinc-100 dark:bg-zinc-800">
+    <div className="flex justify-between space-y-4 flex-col gap-2 p-4 rounded-lg bg-zinc-100 dark:bg-zinc-800">
       <img
-        className=''
         src={image}
         alt={`${product}'s profile`}
       />
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col space-y-1 gap-1">
         <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           {product}
+        </span>
+        <span className='flex'>
+          <Star className='w-4 h-4 text-amber-500' />
+          <Star className='w-4 h-4 text-amber-500' />
+          <Star className='w-4 h-4 text-amber-500' />
+          <Star className='w-4 h-4 text-amber-500' />
+          <Star className='w-4 h-4 text-amber-500' />
         </span>
         <span className="text-sm leading-5 text-zinc-500 dark:text-zinc-400">
           Por apenas:
@@ -23,6 +43,23 @@ export function Product({ image, product, price }) {
         <span className="text-md leading-5 text-zinc-500 dark:text-amber-300">
           {price}
         </span>
+
+        <div className='flex gap-2'>
+          <button
+            type="button"
+            className="text-md px-2 py-1 rounded bg-minha-cor font-medium dark:text-zinc-50"
+            onClick={onBuy}
+          >
+            {buyText}
+          </button>
+          <button
+            type="button"
+            className="text-md font-medium border px-2 py-1 rounded border-minha-cor text-zinc-700 dark:text-minha-cor"
+            onClick={onAddToCart}
+          >
+            {addToCartText}
+          </button>
+        </div>
       </div>
     </div>
   );
